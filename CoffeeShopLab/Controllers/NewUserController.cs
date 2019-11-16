39377d10674621be024065a76e22d2514c3607ed
Mike <michael005@me.com>
2019-11-16 13:02:04 -0500

    First Commit

---
+++ CoffeeShopLab/Controllers/NewUserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class NewUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterUser()
+        {
+                return View();
+        }
+
+        public IActionResult NewUserSummary(NewUser newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(newUser);
+            }
+            else
+            {
+                return View("RegisterUser");
+            }
+
+        }
+
+    }
+}
---
+++ CoffeeShopLab/Models/NewUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopLab.Models
+{
+    public class NewUser
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Username requires min 8 characters")]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(45, MinimumLength = 8, ErrorMessage = "E-Mail requires min 8 characters with valid @.com")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(45, MinimumLength = 8, ErrorMessage = "Password requires min 8 characters")]
+        public string Password { get; set; }
+
+    }
+}
